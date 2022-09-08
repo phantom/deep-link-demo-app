eaaf9f96d0f268bedf446d2ce229b94cb5976193
joku <jokumusic@gmail.com>
2022-09-08 13:32:06 -0700

    Added PhantomWallet class to simplify the interaction with Phantom Wallet. This makes it easy for code reuse throughout an application or even creating an instance inside of a hook.

--- App.tsx
+++ App.tsx
@@ -1,312 +1,132 @@
 import "react-native-get-random-values";
 import "react-native-url-polyfill/auto";
-import { Buffer } from "buffer";
-global.Buffer = global.Buffer || Buffer;
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Button, Platform, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useRef, useState } from "react";
+import { Button, Platform, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import * as Linking from "expo-linking";
-import nacl from "tweetnacl";
-import bs58 from "bs58";
 import {
-  clusterApiUrl,
   Connection,
-  Keypair,
-  PublicKey,
   SystemProgram,
   Transaction,
+  clusterApiUrl
 } from "@solana/web3.js";
 
-const NETWORK = clusterApiUrl("mainnet-beta");
+import PhantomWallet from "./PhantomWallet";
 
-const onConnectRedirectLink = Linking.createURL("onConnect");
-const onDisconnectRedirectLink = Linking.createURL("onDisconnect");
-const onSignAndSendTransactionRedirectLink = Linking.createURL("onSignAndSendTransaction");
-const onSignAllTransactionsRedirectLink = Linking.createURL("onSignAllTransactions");
-const onSignTransactionRedirectLink = Linking.createURL("onSignTransaction");
-const onSignMessageRedirectLink = Linking.createURL("onSignMessage");
+const NETWORK = "devnet";
 
-const buildUrl = (path: string, params: URLSearchParams) =>
-  `https://phantom.app/ul/v1/${path}?${params.toString()}`;
-
-const decryptPayload = (data: string, nonce: string, sharedSecret?: Uint8Array) => {
-  if (!sharedSecret) throw new Error("missing shared secret");
-
-  const decryptedData = nacl.box.open.after(bs58.decode(data), bs58.decode(nonce), sharedSecret);
-  if (!decryptedData) {
-    throw new Error("Unable to decrypt data");
-  }
-  return JSON.parse(Buffer.from(decryptedData).toString("utf8"));
-};
-
-const encryptPayload = (payload: any, sharedSecret?: Uint8Array) => {
-  if (!sharedSecret) throw new Error("missing shared secret");
-
-  const nonce = nacl.randomBytes(24);
-
-  const encryptedPayload = nacl.box.after(
-    Buffer.from(JSON.stringify(payload)),
-    nonce,
-    sharedSecret
-  );
-
-  return [nonce, encryptedPayload];
-};
 
 export default function App() {
-  const [deepLink, setDeepLink] = useState<string>("");
   const [logs, setLogs] = useState<string[]>([]);
-  const connection = new Connection(NETWORK);
-  const addLog = useCallback((log: string) => setLogs((logs) => [...logs, "> " + log]), []);
+  const connection = new Connection(clusterApiUrl(NETWORK));
+  const phantomWallet = useRef(new PhantomWallet(NETWORK)).current;
+  const addLog = useCallback((log: string) => {
+      setLogs((logs) => [...logs, "> " + log]);
+      console.log(log);
+  }, []);
   const scrollViewRef = useRef<any>(null);
 
-  // store dappKeyPair, sharedSecret, session and account SECURELY on device
-  // to avoid having to reconnect users.
-  const [dappKeyPair] = useState(nacl.box.keyPair());
-  const [sharedSecret, setSharedSecret] = useState<Uint8Array>();
-  const [session, setSession] = useState<string>();
-  const [phantomWalletPublicKey, setPhantomWalletPublicKey] = useState<PublicKey>();
-
-  useEffect(() => {
-    (async () => {
-      const initialUrl = await Linking.getInitialURL();
-      if (initialUrl) {
-        setDeepLink(initialUrl);
-      }
-    })();
-    Linking.addEventListener("url", handleDeepLink);
-    return () => {
-      Linking.removeEventListener("url", handleDeepLink);
-    };
-  }, []);
-
-  const handleDeepLink = ({ url }: Linking.EventType) => {
-    setDeepLink(url);
-  };
-
-  // handle inbounds links
-  useEffect(() => {
-    if (!deepLink) return;
-
-    const url = new URL(deepLink);
-    const params = url.searchParams;
-
-    if (params.get("errorCode")) {
-      addLog(JSON.stringify(Object.fromEntries([...params]), null, 2));
-      return;
-    }
-
-    if (/onConnect/.test(url.pathname)) {
-      const sharedSecretDapp = nacl.box.before(
-        bs58.decode(params.get("phantom_encryption_public_key")!),
-        dappKeyPair.secretKey
-      );
-
-      const connectData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecretDapp
-      );
-
-      setSharedSecret(sharedSecretDapp);
-      setSession(connectData.session);
-      setPhantomWalletPublicKey(new PublicKey(connectData.public_key));
-
-      addLog(JSON.stringify(connectData, null, 2));
-    } else if (/onDisconnect/.test(url.pathname)) {
-      addLog("Disconnected!");
-    } else if (/onSignAndSendTransaction/.test(url.pathname)) {
-      const signAndSendTransactionData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecret
-      );
-
-      addLog(JSON.stringify(signAndSendTransactionData, null, 2));
-    } else if (/onSignAllTransactions/.test(url.pathname)) {
-      const signAllTransactionsData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecret
-      );
-
-      const decodedTransactions = signAllTransactionsData.transactions.map((t: string) =>
-        Transaction.from(bs58.decode(t))
-      );
-
-      addLog(JSON.stringify(decodedTransactions, null, 2));
-    } else if (/onSignTransaction/.test(url.pathname)) {
-      const signTransactionData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecret
-      );
-
-      const decodedTransaction = Transaction.from(bs58.decode(signTransactionData.transaction));
-
-      addLog(JSON.stringify(decodedTransaction, null, 2));
-    } else if (/onSignMessage/.test(url.pathname)) {
-      const signMessageData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecret
-      );
-
-      addLog(JSON.stringify(signMessageData, null, 2));
-    }
-  }, [deepLink]);
-
+ 
   const createTransferTransaction = async () => {
-    if (!phantomWalletPublicKey) throw new Error("missing public key from user");
-    let transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: phantomWalletPublicKey,
-        toPubkey: phantomWalletPublicKey,
-        lamports: 100,
-      })
-    );
-    transaction.feePayer = phantomWalletPublicKey;
-    addLog("Getting recent blockhash");
-    const anyTransaction: any = transaction;
-    anyTransaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-    return transaction;
+    return new Promise<Transaction>(async (resolve,reject)=>{
+      const phantomWalletPublicKey = phantomWallet.getWalletPublicKey();
+      if(!phantomWalletPublicKey) {
+        reject('Not connected to a wallet');
+        return;
+      }
+
+      let transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: phantomWalletPublicKey,
+          toPubkey: phantomWalletPublicKey,
+          lamports: 100,
+        })
+      );
+      transaction.feePayer = phantomWalletPublicKey;
+      addLog('Getting recent blockhash');
+      const anyTransaction: any = transaction;
+      anyTransaction.recentBlockhash = (await connection.getLatestBlockhash().catch(err=>reject(err))).blockhash;
+      resolve(transaction);
+    });
   };
 
   const connect = async () => {
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      cluster: "mainnet-beta",
-      app_url: "https://phantom.app",
-      redirect_link: onConnectRedirectLink,
-    });
-
-    const url = buildUrl("connect", params);
-    Linking.openURL(url);
+    addLog('Connecting...');
+    phantomWallet
+    .connect()
+    .then(()=> addLog('connected!'))
+    .catch(error=>addLog(error));
   };
 
   const disconnect = async () => {
-    const payload = {
-      session,
-    };
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onDisconnectRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-
-    const url = buildUrl("disconnect", params);
-    Linking.openURL(url);
+    addLog('Disconnecting...');
+    phantomWallet
+    .disconnect()
+    .then(()=> addLog('disconnected!'))
+    .catch(error=>addLog(error));    
   };
 
   const signAndSendTransaction = async () => {
-    const transaction = await createTransferTransaction();
-
-    const serializedTransaction = transaction.serialize({
-      requireAllSignatures: false,
+    const transaction = await createTransferTransaction()
+    .catch(err=>{
+      addLog(err)
     });
 
-    const payload = {
-      session,
-      transaction: bs58.encode(serializedTransaction),
-    };
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
+    if(!transaction)
+      return;
 
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onSignAndSendTransactionRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
+    addLog('Signing and sending transaction...');
 
-    addLog("Sending transaction...");
-    const url = buildUrl("signAndSendTransaction", params);
-    Linking.openURL(url);
+    phantomWallet
+    .signAndSendTransaction(transaction, false)
+    .then((t)=> addLog(`signAndSendTransaction result: ${JSON.stringify(t)}`))
+    .catch(error=>addLog(error));
   };
 
   const signAllTransactions = async () => {
     const transactions = await Promise.all([
       createTransferTransaction(),
       createTransferTransaction(),
-    ]);
+    ])
+    .catch(err=>addLog(err));
 
-    const serializedTransactions = transactions.map((t) =>
-      bs58.encode(
-        t.serialize({
-          requireAllSignatures: false,
-        })
-      )
-    );
+    if(!transactions)
+      return;
 
-    const payload = {
-      session,
-      transactions: serializedTransactions,
-    };
+    addLog('Signing multiple transactions...');
 
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onSignAllTransactionsRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-
-    addLog("Signing transactions...");
-    const url = buildUrl("signAllTransactions", params);
-    Linking.openURL(url);
+    phantomWallet
+    .signAllTransactions(transactions, false)
+    .then(ts=>{
+      ts.map(t=>{
+        addLog(`transaction: ${JSON.stringify(t)}`);
+      })
+    })
+    .catch(error=>addLog(error));
   };
 
   const signTransaction = async () => {
-    const transaction = await createTransferTransaction();
+    addLog('Signing transaction...');
+    const transaction = await createTransferTransaction()
+    .catch(addLog);
+    
+    if(!transaction)
+      return;
+    
 
-    const serializedTransaction = bs58.encode(
-      transaction.serialize({
-        requireAllSignatures: false,
-      })
-    );
-
-    const payload = {
-      session,
-      transaction: serializedTransaction,
-    };
-
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onSignTransactionRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-
-    addLog("Signing transaction...");
-    const url = buildUrl("signTransaction", params);
-    Linking.openURL(url);
+    phantomWallet
+    .signTransaction(transaction, false)
+    .then(t=>addLog(`signed transaction: ${JSON.stringify(t)}`))
+    .catch(error=>addLog(error));
   };
 
   const signMessage = async () => {
     const message = "To avoid digital dognappers, sign below to authenticate with CryptoCorgis.";
+    addLog('Signing message...');
 
-    const payload = {
-      session,
-      message: bs58.encode(Buffer.from(message)),
-    };
-
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onSignMessageRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-
-    addLog("Signing message...");
-    const url = buildUrl("signMessage", params);
-    Linking.openURL(url);
+    phantomWallet
+    .signMessage(message)
+    .then((transaction)=> addLog(`signed message: ${JSON.stringify(transaction)}`))
+    .catch(error=>addLog(error))    
   };
 
   return (
